be08cd081dda0a225d69f336b24777e955018b90
Gabrie345 <gabriel345wp@gmail.com>
2022-12-24 01:18:57 -0300

    log4j e logger controller

--- src/main/java/com/gabrie345/msclientes/application/ClientesController.java
+++ src/main/java/com/gabrie345/msclientes/application/ClientesController.java
@@ -2,9 +2,9 @@ package com.gabrie345.msclientes.application;
 
 import java.util.Optional;
 
-import org.hibernate.annotations.Parameter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,16 +19,24 @@ import org.springframework.web.bind.annotation.RestController;
 import com.gabrie345.msclientes.application.dto.ClienteSaveRequestDto;
 import com.gabrie345.msclientes.domain.Cliente;
 
+import lombok.extern.slf4j.Slf4j;
+
+	
+
 @RestController
 @RequestMapping(value="cliente")
-
+@Slf4j
 public class ClientesController {
 	
 	@Autowired
 	private ClienteService service;
 	
+	@Autowired
+	  private static Logger logger = LoggerFactory.getLogger(Slf4j.class);
+	
 	@RequestMapping(method = RequestMethod.GET)
 	public String status() {
+        logger.info("Info log message");
 		return "ok";
 	}
 	
